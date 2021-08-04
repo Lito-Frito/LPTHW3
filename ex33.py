e53e8d428bb914a2d35a67fe4d524c152c0d8d6a
crc8109 <crc8109@pm.me>
2021-08-04 09:12:18 -0400

    Add note for future reference

--- ex33.py
+++ ex33.py
@@ -5,6 +5,7 @@ while i < 4:
     print(f"At the top i is {i}")
     numbers.append(i)
 
+    #  Increments i by 1 each iteration
     i += 1
     print("Numbers now: ", numbers)
     print(f"At the bottom i is {i}")
